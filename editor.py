8dc84d51741734de6358c9617de8795f670f8940
Ivo Gioffre <dzurlo@alumno.huergo.edu.ar>
2025-10-29 09:01:41 -0300

    nuevas cositas

--- editor.py
+++ editor.py
@@ -35,7 +35,7 @@ class Editor:
         self.tilemap = Tilemap(self, tile_size=16)
 
         try: 
-            self.tilemap.load("base.json")
+            self.tilemap.load("5.json")
 
 
         except FileNotFoundError:
