24aa68b02198c29ae5aa0aeafac29f7866778bb4
sabiondo <jgiuri@alumno.huergo.edu.ar>
2025-11-12 11:09:37 -0300

    adadad

--- editor.py
+++ editor.py
@@ -37,7 +37,7 @@ class Editor:
         self.tilemap = Tilemap(self, tile_size=16)
 
         try: 
-            self.tilemap.load("10.json")
+            self.tilemap.load("5.json")
 
         except FileNotFoundError:
             pass
@@ -138,7 +138,7 @@ class Editor:
                     if event.key == pygame.K_t :
                         self.tilemap.autotile()
                     if event.key == pygame.K_o :
-                        self.tilemap.save('maps/10.json')
+                        self.tilemap.save('maps/5.json')
 
 
                 if event.type == pygame.KEYUP:
