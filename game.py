83d48f7b23ebf10d460f56d859c5685db8ee618d
sabiondo298 <jgiuri@alumno.huergo.edu.ar>
2025-11-24 19:24:55 -0300

    a

--- game.py
+++ game.py
@@ -35,7 +35,7 @@ class Game:
             10: {'max_time': 90, 'start_pos': [0, 115], 'background' : 'epico_bg'},
             11: {'max_time': 60, 'start_pos': [0, 115], 'background' : 'ciudadnoche_bg'},
             12: {'max_time': 60, 'start_pos': [0, 115], 'background' : 'alcantarilla_bg'},
-            13: {'max_time': 40, 'start_pos': [0, 115], 'background' : 'bosque_bg'},
+            13: {'max_time': 60, 'start_pos': [0, 115], 'background' : 'bosque_bg'},
     
             
 
